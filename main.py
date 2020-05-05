903b3eb2b288e6a71aa8fe51826fb7ffdaf91e8c
uusijo <jorgen.barkelius@delaval.com>
2020-05-05 21:17:48 +0200

    added some more

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 """
 Test file for practicing Git cherry picking.
 """
-name = input('Enter your name, please!')
+
+name = input('Enter your name, please! ')
 print(f"Hello {name}!")
+print(f"Why don't you learn something new today {name}?")
 
